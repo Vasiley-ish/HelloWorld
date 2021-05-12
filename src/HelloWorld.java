8487d2ef848d3c71eea6ed9b54b01b7ea6bbf296
Vacily <vacileypds@mai.ru>
2021-05-12 11:57:39 +0400

    add HelloWorld class

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int i=1;
+        System.out.println(i+".");
+        i+=1;
+        System.out.println(i+"..");
+        i+=1;
+        System.out.println(i+"...");
+        System.out.println("hello world");
+    }
+}
